cf6f407e6d5348fc184c1d71ff77ec9732ddd36c
Steven Vergenz <1882376+stevenvergenz@users.noreply.github.com>
2024-10-11 21:02:40 -0700

    Finish core loop

--- robot/src/spiderbot.rs
+++ robot/src/spiderbot.rs
@@ -1,37 +1,49 @@
-use arduino_hal::prelude::*;
+use arduino_hal::{
+    hal::port::{PD0, PD1},
+    pac::{TC0, USART0},
+    port::{
+        mode::{Input, Output},
+        Pin,
+    },
+    prelude::*
+};
 use crate::millis::{millis, millis_init};
 
 pub struct SpiderBot {
-    dp: arduino_hal::Peripherals,
+    clock_pin: TC0,
+    serial: arduino_hal::Usart<USART0, Pin<Input, PD0>, Pin<Output, PD1>>,
 }
 
 impl SpiderBot {
     pub fn new() -> Self {
+        let dp = arduino_hal::Peripherals::take().unwrap();
+        let pins = arduino_hal::pins!(dp);
+        let serial = arduino_hal::default_serial!(dp, pins, 57600);
+
         Self {
-            dp: arduino_hal::Peripherals::take().unwrap(),
+            clock_pin: dp.TC0,
+            serial,
         }
     }
 
     pub fn exec(&mut self, freq_hz: usize) -> ! {
-
-        millis_init(&self.dp.TC0);
+        // set up the throttle checker
+        millis_init(&self.clock_pin);
 
         // Enable interrupts globally
         unsafe { avr_device::interrupt::enable() };
 
-        Self::throttle(20, self.tick)
-    }
-
-    fn throttle(freq_hz: usize, body: FnMut) -> ! {
         let time_budget = 1000 / freq_hz;
         loop {
             let time_start = millis();
 
-            body();
+            self.tick();
 
-            let runtime_ms = millis() - time_start;
-            if runtime_ms < time_budget {
-                arduino_hal::delay_ms((time_budget - runtime_ms) as u16);
+            let runtime = millis() - time_start;
+            if runtime < time_budget {
+                let delay = time_budget - runtime;
+                ufmt::uwriteln!(self.serial, "Waiting {}ms", delay).unwrap_infallible();
+                arduino_hal::delay_ms((time_budget - runtime) as u16);
             }
         }
     }
